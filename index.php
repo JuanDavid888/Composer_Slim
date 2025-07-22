3e522cb48eadabf142198d5b919cd3e6fce4473a
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-07-22 14:18:53 +0000

    feat: :sparkles: Se aseguró el metodo GET
    Se utilizo el metodo con parametros

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use App\Middleware\JsonBodyParserMiddleware;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as Handler;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get('/', function(Request $req, Response $res, array $args) {
+    $res->getBody()->write(json_encode(["message" => "Hola desde Slim"]));
+    return $res;
+});
+// Middlewares
+// Capa que actua entre la solicitud y la respuesta
+// Ayuda a modificar o intersectar (validar)
+
+// GET /campers
+// POST /campers
+// PUT /campers/1
+// PATH /campers/1
+// DELETE /campers/1
+
+$app->get("/campers/{name}/{skill}", function(Request $req, Response $res, array $args) {
+    // GET localhost:8081/campers/Adrian/php
+    $name = $args["name"];
+    $skill = $args["skill"];
+
+    $res->getBody()->write(json_encode([$name, $skill]));
+    return $res;
+})->add(function(Request $req, Handler $han): Response {
+    $response = $han->handle($req);
+    return $response->withHeader('X-Powered-By', 'Slim Framework');
+}); // Aplica un metodo especifico a esta funcion get
+
+$app->get("/campers", function(Request $req, Response $res, array $args) {
+    // GET localhost:8081/campers?name=Adrian&skill=php
+    $params = $req->getQueryParams();
+    $name = $params["name"] ?? "default";
+    $skill = $params["skill"] ?? "default";
+
+    $res->getBody()->write(json_encode([$name, $skill]));
+    return $res;
+});
+
+$app->run();
